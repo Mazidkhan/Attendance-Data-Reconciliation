c3af33e97d98cf33f71e0ff07686638f8d4d1445
Mazidulla <mazidula.mk@gmail.com>
2024-08-18 21:13:21 +0530

    Added changes

--- src/attendance_reconciliation/default/prod.py
+++ src/attendance_reconciliation/default/prod.py
@@ -3,10 +3,6 @@
 #  Proprietary and confidential
 #  Written by VAA Technologies Private Limited
 
-import os
-rpi_user = 'vaa' if os.path.isdir('/home/vaa') else 'pi'
-
-PROD_LOG_PATH = '/home/{}/data-drive/attendance-data-reconciliation/logs'.format(rpi_user)
-PROD_CONFIG_PATH = '/home/{}/data-drive/attendance-data-reconciliation/files/config'.format(rpi_user)
-PROD_DB_PATH = '/home/{}/data-drive/attendance-data-reconciliation/files/db'.format(rpi_user)
-
+PROD_LOG_PATH = '/data-drive/attendance-data-reconciliation/logs'
+PROD_CONFIG_PATH = '/data-drive/attendance-data-reconciliation/files/config'
+PROD_DB_PATH = '/data-drive/attendance-data-reconciliation/files/db'
